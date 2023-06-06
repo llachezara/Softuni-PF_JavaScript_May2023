4673d5c6ff71093b95029cf91890ac10ef25b0fb
Lachezara <93489868+llachezara@users.noreply.github.com>
2023-06-06 15:26:15 +0300

    Added 2nd solution to 10_factorialDivision.js

--- Exercise Functions/10_factorialDivision.js
+++ Exercise Functions/10_factorialDivision.js
@@ -3,7 +3,7 @@ function factorialDivision(firstNum, secondNum) {
 
     function calcFactorial(num) {
         let sum = 1;
-        
+
         for (let i = 1; i <= num; i++) {
             sum *= i;
         }
@@ -11,6 +11,19 @@ function factorialDivision(firstNum, secondNum) {
         return sum;
     }
 
-    return result.toFixed(2); 
+    return result.toFixed(2);
 }
-console.log(factorialDivision(6, 2))
+
+function factorialDiv(num1, num2) {
+    let result = factorialCalc(num1) / factorialCalc(num2);
+
+    function factorialCalc(num) {
+        if (num === 0) {
+            return 1;
+        } else {
+            return num * factorialCalc(num - 1);
+        }
+    }
+    return result.toFixed(2);
+}
+console.log(factorialDivision(6,2), factorialDiv(6, 2));
